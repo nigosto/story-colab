1472931741e563c8a9a2cdad9e6ac2fd0173a204
simo1209 <simeongeorgiev09@gmail.com>
2023-03-17 19:45:36 +0200

    Added proxying to wsServer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/ws',
+        destination: `http://localhost:5000`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
