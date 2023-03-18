0dbb57ca0b0f451e33c0e07d13d2df5955221cdf
simo1209 <simeongeorgiev09@gmail.com>
2023-03-18 13:57:10 +0200

    Fixed bot turn_event

--- wsServer.mjs
+++ wsServer.mjs
@@ -2,14 +2,14 @@ import mongoose from 'mongoose';
 import { Configuration, OpenAIApi } from 'openai';
 import { WebSocketServer } from 'ws';
 
-import Room from "./src/models/Room.js"
+// import Room from "./src/models/Room.js"
 
-await mongoose.connect('mongodb://192.168.105.14:27017/story-colab');
+await mongoose.connect(process.env.MONGODB_URI);
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
+const openai = new OpenAIApi(configuration);
 
 const wss = new WebSocketServer({ port: process.env.WS_PORT });
 
@@ -34,30 +34,58 @@ const MESSAGE_HANDLER = {
     }
   },
   "game_started": async function (ws, room, msg) {
-    room.details = await Room.find({ _id: msg["room_id"] });
-    console.log(room.details);
+    // room.details = await Room.find({ _id: msg["room_id"] });
+    room["description"] = msg["description"];
+    console.log(room);
   },
   "disconnect": async function (ws, room, msg) {
     console.log("disconnected", msg);
   },
-  "character_line": async function (ws, room, msg) {
-    console.log("character line", msg);
+  "game_ended": async function (ws, room, msg) {
+    console.log("disconnected", msg);
   },
-  "turn": async function (ws, room, msg) {
-    console.log("turn", msg);
-    let userIdForTurn = msg["turn_user"];
-    let userForTurn = room.details.participants.find(prt => prt.user_id === userIdForTurn);
-    if (msg.username === "simoBot" || userForTurn.isBot) {
+  "added_bot": async function (ws, room, msg) {
+    room["bots"] = msg["bots"];
+  },
+  "character_line": async function (ws, room, msg){
+    let role = msg["role"];
+    let text = msg["text"];
+    
+    msg.turn = room.turn;
+    if ( msg["username"] !== msg.turn ) {
+      return;      
+    }
+
+    let messages = room['messages'] = room['messages'] ?? [];
+    messages.push({role, text});
+  },
+  "turn_event": async function (ws, room, msg) {
+    let usernameForTurn = room.usernameForTurn = msg["username"];
+    let roleForTurn = room.roleForTurn = msg["role"];
+
+    room.turn = usernameForTurn;
+
+    // let roomParticipants = Room.find({_id: room["room_id"]}).participants;
+    // let userForTurnRole = roomParticipants.find(rPrt => rPrt.name === userForTurn.username)?.role;
+
+    if (usernameForTurn.startsWith("Bot")) {
+      let botPrompt = room.messages ? room.messages.map(message => `${message.role}: ${message.text}`).join('\n') : "";
+      if (botPrompt.length > 0) {
+        botPrompt += '\n';
+      }
+      botPrompt += `${roleForTurn}: `;
+      console.log("Bot prompt '%s'", botPrompt);
       const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend: Yes, I watched a classic horror film from the 1950s. It was really good.\nYou: What was it about?\nFriend: It was about a group of teenagers who are terrorized by a mysterious creature in the woods. It was really suspenseful and had some great special effects for its time.\nYou: That sounds like a great movie! Do you recommend it?\nFriend: Absolutely! It's a  classic and I think anyone who loves horror films should watch it.\nYou: I will. And did you listen to an interesting song recently?\nFriend:  Yes, I recently discovered a song by a band called The National. It's called \"Fake Empire\" and it's really great. I highly recommend it!",
+        prompt: botPrompt,
         temperature: 0.5,
         max_tokens: 50,
         top_p: 1,
         frequency_penalty: 0.5,
         presence_penalty: 0,
-        stop: ["You:"],
+        stop: [`${roleForTurn}:`],
       });
+      console.log("Bot response '%s'", JSON.stringify(response.data.choices));
     }
   }
 }
@@ -73,7 +101,7 @@ wss.on('connection', function connection(ws) {
       room_id: ws.participant.room_id,
       user_id: ws.participant.user_id,
       username: ws.participant.username,
-      idx: ws.participant.idx,
+      // idx: ws.participant.idx,
     }
     console.log('closing', code, reason, participant);
     roomPool[participant.room_id].participants.splice(roomPool[participant.room_id].participants.findIndex(prt => prt.user_id === participant.user_id), 1);
