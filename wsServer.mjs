acfe4249b7ce9b215dd98bc890f1eed7fe3b09e4
simo1209 <simeongeorgiev09@gmail.com>
2023-03-18 05:50:28 +0200

    ai bot example

--- wsServer.mjs
+++ wsServer.mjs
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
+import { Configuration, OpenAIApi } from 'openai';
 import { WebSocketServer } from 'ws';
 
 import Room from "./src/models/Room.js"
 
 await mongoose.connect('mongodb://192.168.105.14:27017/story-colab');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 
 const wss = new WebSocketServer({ port: process.env.WS_PORT });
 
@@ -33,6 +39,26 @@ const MESSAGE_HANDLER = {
   },
   "disconnect": async function (ws, room, msg) {
     console.log("disconnected", msg);
+  },
+  "character_line": async function (ws, room, msg) {
+    console.log("character line", msg);
+  },
+  "turn": async function (ws, room, msg) {
+    console.log("turn", msg);
+    let userIdForTurn = msg["turn_user"];
+    let userForTurn = room.details.participants.find(prt => prt.user_id === userIdForTurn);
+    if (msg.username === "simoBot" || userForTurn.isBot) {
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend: Yes, I watched a classic horror film from the 1950s. It was really good.\nYou: What was it about?\nFriend: It was about a group of teenagers who are terrorized by a mysterious creature in the woods. It was really suspenseful and had some great special effects for its time.\nYou: That sounds like a great movie! Do you recommend it?\nFriend: Absolutely! It's a  classic and I think anyone who loves horror films should watch it.\nYou: I will. And did you listen to an interesting song recently?\nFriend:  Yes, I recently discovered a song by a band called The National. It's called \"Fake Empire\" and it's really great. I highly recommend it!",
+        temperature: 0.5,
+        max_tokens: 50,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+        stop: ["You:"],
+      });
+    }
   }
 }
 
@@ -50,7 +76,7 @@ wss.on('connection', function connection(ws) {
       idx: ws.participant.idx,
     }
     console.log('closing', code, reason, participant);
-    roomPool[participant.room_id].participants.splice(roomPool[participant.room_id].participants.findIndex(prt=>prt.user_id === participant.user_id), 1);
+    roomPool[participant.room_id].participants.splice(roomPool[participant.room_id].participants.findIndex(prt => prt.user_id === participant.user_id), 1);
     ws.emit("message", JSON.stringify({
       ...participant,
       "type": "disconnect"
@@ -92,12 +118,12 @@ wss.on('connection', function connection(ws) {
       return;
     }
 
-    if (!MESSAGE_HANDLER[msg.type]) {
-      console.error("Unsupoorted error", data.toString());
+    // if (!MESSAGE_HANDLER[msg.type]) {
+    //   console.error("Unsupoorted error", data.toString());
 
-      ws.close(4210, `Message type is not supported.`);
-      return;
-    }
+    //   ws.close(4210, `Message type is not supported.`);
+    //   return;
+    // }
 
     let room = roomPool[msg["room_id"]];
     if (!room) {
@@ -105,7 +131,9 @@ wss.on('connection', function connection(ws) {
     }
 
     try {
-      await MESSAGE_HANDLER[msg["type"]](ws, room, msg)
+      if (MESSAGE_HANDLER[msg["type"]]) {
+        await MESSAGE_HANDLER[msg["type"]](ws, room, msg)
+      }
     } catch (e) {
       console.error(e, msg);
       ws.close(1013, "Unexpected error");
