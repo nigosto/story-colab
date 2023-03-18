978a62c3be4c9c9618b580d4b8075283ba0aa7be
simo1209 <simeongeorgiev09@gmail.com>
2023-03-18 21:42:09 +0200

    Drag for example chat bubble

--- src/pages/story/[id].js
+++ src/pages/story/[id].js
@@ -1,3 +1,65 @@
+import { useRouter } from "next/router";
+import React from "react";
+import dynamic from 'next/dynamic'
+const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+    ssr: false,
+})
+
 export default function Story() {
-    return <div>Story</div>
+
+    // const router = useRouter();
+
+    // let hasCanvas = false;
+    let image;
+    let chatBubbleImg;
+
+    let bubbleX = 50, bubbleY = 50;
+
+    const setup = async (p5, canvasParentRef) => {
+        // if (hasCanvas) {
+        //     return;
+        // }
+        // const { id } = router.query;
+        // console.log(id);
+        // const roomRes = await fetch(`http://localhost:3000/api/room/get/${id}`);
+        // const room = (await roomRes.json()).room;
+        // console.log(room);
+        p5.createCanvas(500, 500).parent(canvasParentRef);
+        // hasCanvas = true;
+        // p5.background(0);
+        // p5.ellipse(50, 50, 70, 70);
+        image = p5.loadImage("https://media.istockphoto.com/id/1267021092/photo/funny-winking-kitten.jpg?s=612x612&w=0&k=20&c=9PoFYkqKZ30F_ubxX90_azwsR22ENwrFnOjxV0RaoTo=");
+        chatBubbleImg = p5.loadImage("https://static.vecteezy.com/system/resources/previews/001/195/458/original/speech-bubble-png.png");
+        console.log(image, chatBubbleImg);
+        p5.textSize(24);
+        p5.textAlign(p5.CENTER);
+
+        // image = p5.loadImage(room.image);
+    };
+
+    const draw = (p5) => {
+
+        p5.background(0);
+        // p5.ellipse(x, y, 70, 70);
+        if (image) {
+            p5.image(image, 0, 0);
+            p5.image(chatBubbleImg, bubbleX, bubbleY, 204, 154);
+            p5.text('Meow', bubbleX + 102, bubbleY + 77);
+        }
+        // x++;
+    };
+
+    const mouseClicked = (_p5, event) => {
+        // console.log(event)
+    }
+
+    const mouseDragged = (_p5, event) => {
+        // console.log(event)
+        bubbleX = _p5.mouseX - 102;
+        bubbleY = _p5.mouseY - 77;
+
+    }
+
+    return <Sketch style={{"margin-top": "3rem"}} setup={setup} draw={draw} mouseDragged={mouseDragged} mouseClicked={mouseClicked}/>;
+
 }
