83d4de6cfb09594fdb9532d54f3de1f8ef6cb6e6
nigosto <nigosto@gmail.com>
2023-03-19 11:56:30 +0200

    merge

--- src/pages/story/[id].js
+++ src/pages/story/[id].js
@@ -22,44 +22,49 @@ export async function getServerSideProps(context) {
 }
 
 export default function Story({ messages, imageSrc, participants }) {
-    let bubbleWidth = 204;
-    let bubbleHeight = 154;
-    let image;
-    let chatBubbleImg;
-    let chatBubbles = [];
-    let cnv;
-    let draggedBubble;
+  let bubbleWidth = 204;
+  let bubbleHeight = 154;
+  let image;
+  let chatBubbleImg;
+  let chatBubbles = [];
+  let cnv;
+  let draggedBubble;
+  let pageNum = 0;
 
   // let bubbleX = window.innerWidth / 2,
   //     bubbleY = window.innerHeight / 2;
 
   const setup = async (p5, canvasParentRef) => {
     // if (!cnv) {
-    if(!globalP5) {
-      globalP5 = p5
-    }
+    console.log("simo1");
     cnv = p5
       .createCanvas(window.innerWidth, window.innerHeight)
       .parent(canvasParentRef);
 
-        if (!cnv) {
-            console.log("simo1");
-            cnv = p5.createCanvas(window.innerWidth, window.innerHeight).parent(
-                canvasParentRef
-            );
-            for (let i = 0; i < participants.length; i++) {
-                console.log("i", i);
-                chatBubbles.push({ scale: 1.0, bubbleX: window.innerWidth / 2 + i * 100, bubbleY: window.innerHeight / 2, isFlip: false, message: messages[i].split(': ')[1] });
-            }
-            draggedBubble = chatBubbles[0];
-            console.log("simo1", draggedBubble);
-
-        }
-        image = p5.loadImage(imageSrc);
-        chatBubbleImg = p5.loadImage(
-            "https://static.vecteezy.com/system/resources/previews/001/195/458/original/speech-bubble-png.png"
+    if (chatBubbles.length === 0) {
+      for (let i = 0; i < participants.length; i++) {
+        console.log("i", i);
+        let participantMessages = messages.filter((message) =>
+          message.startsWith(participants[i].role)
         );
+        chatBubbles.push({
+          scale: 1.0,
+          bubbleX: window.innerWidth / 2 + i * 100,
+          bubbleY: window.innerHeight / 2,
+          isFlip: false,
+          messages: participantMessages,
+        });
+      }
+      draggedBubble = chatBubbles[0];
+    }
 
+    console.log("simo1", draggedBubble);
+
+    // }
+    image = p5.loadImage(imageSrc);
+    chatBubbleImg = p5.loadImage(
+      "https://static.vecteezy.com/system/resources/previews/001/195/458/original/speech-bubble-png.png"
+    );
 
     p5.textSize(24);
     p5.textAlign(p5.CENTER);
@@ -77,18 +82,24 @@ export default function Story({ messages, imageSrc, participants }) {
       );
     }
 
-        for (const chatBubble of chatBubbles) {
-            let { bubbleX, bubbleY, isFlip, message } = chatBubble;
+    for (const chatBubble of chatBubbles) {
+      let { bubbleX, bubbleY, isFlip, messages } = chatBubble;
 
-            if (isFlip) {
-                p5.scale(-1, 1);
-            } else {
-                p5.scale(1, 1);
-            }
-            p5.image(chatBubbleImg, bubbleX, bubbleY, bubbleWidth, bubbleHeight);
-            p5.text(message, bubbleX + bubbleWidth / 2, bubbleY + bubbleHeight / 2);
+      if (messages[pageNum]) {
+        if (isFlip) {
+          p5.scale(-1, 1);
+        } else {
+          p5.scale(1, 1);
         }
-    };
+        p5.image(chatBubbleImg, bubbleX, bubbleY, bubbleWidth, bubbleHeight);
+        p5.text(
+          messages[pageNum].split(": ")[1],
+          bubbleX + bubbleWidth / 2,
+          bubbleY + bubbleHeight / 2
+        );
+      }
+    }
+  };
 
   const mousePressed = (_p5, event) => {
     let mouseX = event.clientX,
@@ -123,40 +134,70 @@ export default function Story({ messages, imageSrc, participants }) {
 
     draggedBubble.bubbleX = mouseX - draggedBubble.deltaX;
     draggedBubble.bubbleY = mouseY - draggedBubble.deltaY;
+    console.log("new bubble coords", draggedBubble);
   };
 
-    return (
-        <section className={styles.storyPage}>
-            <Button
-                className={styles.resizePlus}
-                type="primary"
-                onClick={() => {
-                    bubbleWidth = bubbleWidth * 1.2;
-                    bubbleHeight = bubbleHeight * 1.2;
-                }}
-            >
-                +
-            </Button>
-            <Button
-                className={styles.resizeMinus}
-                type="primary"
-                danger
-                onClick={() => {
-                    bubbleWidth = bubbleWidth * 0.8;
-                    bubbleHeight = bubbleHeight * 0.8;
-                }}
-            >
-                -
-            </Button>
-            <Button className={styles.flipButton} type="primary" onClick={() => {
-                draggedBubble.isFlip=!draggedBubble.isFlip;
-            }}>Flip</Button>
-            <Sketch
-                setup={setup}
-                draw={draw}
-                mouseDragged={mouseDragged}
-                mousePressed={mousePressed}
-            />
-        </section>
-    );
+  return (
+    <section className={styles.storyPage}>
+      <Button
+        className={styles.resizePlus}
+        type="primary"
+        onClick={() => {
+          bubbleWidth = bubbleWidth * 1.2;
+          bubbleHeight = bubbleHeight * 1.2;
+        }}
+      >
+        +
+      </Button>
+      <Button
+        className={styles.resizeMinus}
+        type="primary"
+        danger
+        onClick={() => {
+          bubbleWidth = bubbleWidth * 0.8;
+          bubbleHeight = bubbleHeight * 0.8;
+        }}
+      >
+        -
+      </Button>
+      <Button
+        className={styles.flipButton}
+        type="primary"
+        onClick={() => {
+          draggedBubble.isFlip = !draggedBubble.isFlip;
+        }}
+      >
+        Flip
+      </Button>
+
+      <Button
+        type="primary"
+        onClick={() => {
+          if (pageNum < participants.length - 1) {
+            pageNum++;
+          }
+        }}
+      >
+        Next
+      </Button>
+
+      <Button
+        type="primary"
+        onClick={() => {
+          if (pageNum > 0) {
+            pageNum--;
+          }
+        }}
+      >
+        Previous
+      </Button>
+
+      <Sketch
+        setup={setup}
+        draw={draw}
+        mouseDragged={mouseDragged}
+        mousePressed={mousePressed}
+      />
+    </section>
+  );
 }
